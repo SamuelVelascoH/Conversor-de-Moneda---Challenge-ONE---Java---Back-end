89ce19fc4da6eb84b9d97430c148107412942793
Samuel Velasco <106352774+SamuelVelascoH@users.noreply.github.com>
2024-04-20 12:07:15 -0600

    Update Principal.java

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,8 +1,8 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal extends Consulta {
     private static String separador = "*************************************";
+    private static String inCantidad = "Escriba la cantidad que quiere convertir: ";
 
     private static void mostrarMenu(){
         System.out.println("Elija un numero en el menu de conversiones: ");
@@ -15,6 +15,16 @@ public class Principal extends Consulta {
         System.out.println("7. Salir");
     }
 
+    public static void ejecutarConversion(String tipoDeMonedaString, Integer numeroMenu, Scanner lectura, Consulta consulta, String sms, String tipo1, String tipo2){
+        tipoDeMonedaString = numeroMenu.toString().replace(numeroMenu.toString(),tipo2);
+        System.out.println("Escriba la cantidad que quiere convertir: ");
+        var amount = Double.valueOf(lectura.nextLine());
+        TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda(tipo1,tipoDeMonedaString, amount);
+        System.out.println(amount+sms+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
+        mostrarMenu();
+        numeroMenu = Integer.valueOf(lectura.nextLine());
+    }
+
     public static void main(String[] args) {
         try {
             Consulta consulta = new Consulta();
@@ -24,53 +34,17 @@ public class Principal extends Consulta {
             while (numeroMenu != 7){
                 var tipoDeMonedaString = "";
                 if (numeroMenu == 1){
-                    tipoDeMonedaString = numeroMenu.toString().replace("1","ARS");
-                    System.out.println("Escriba la cantidad que quiere convertir: ");
-                    var amount = Double.valueOf(lectura.nextLine());
-                    TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda("USD",tipoDeMonedaString, amount);
-                    System.out.println(amount+" USD equivalen a: "+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
-                    mostrarMenu();
-                    numeroMenu = Integer.valueOf(lectura.nextLine());
+                    ejecutarConversion(tipoDeMonedaString, numeroMenu, lectura, consulta, " USD equivalen a: ", "USD", "ARS");
                 } else if (numeroMenu == 2) {
-                    tipoDeMonedaString = numeroMenu.toString().replace("2","USD");
-                    System.out.println("Escriba la cantidad que quiere convertir: ");
-                    var amount = Double.valueOf(lectura.nextLine());
-                    TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda("ARS",tipoDeMonedaString, amount);
-                    System.out.println(amount+" ARS equivalen a: "+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
-                    mostrarMenu();
-                    numeroMenu = Integer.valueOf(lectura.nextLine());
+                    ejecutarConversion(tipoDeMonedaString, numeroMenu, lectura, consulta, " ARS equivalen a: ", "ARS", "USD");
                 } else if (numeroMenu == 3) {
-                    tipoDeMonedaString = numeroMenu.toString().replace("3","BRL");
-                    System.out.println("Escriba la cantidad que quiere convertir: ");
-                    var amount = Double.valueOf(lectura.nextLine());
-                    TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda("USD",tipoDeMonedaString, amount);
-                    System.out.println(amount+" USD equivalen a: "+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
-                    mostrarMenu();
-                    numeroMenu = Integer.valueOf(lectura.nextLine());
+                    ejecutarConversion(tipoDeMonedaString, numeroMenu, lectura, consulta, " USD equivalen a: ", "USD", "BRL");
                 } else if (numeroMenu == 4) {
-                    tipoDeMonedaString = numeroMenu.toString().replace("4","USD");
-                    System.out.println("Escriba la cantidad que quiere convertir: ");
-                    var amount = Double.valueOf(lectura.nextLine());
-                    TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda("BRL",tipoDeMonedaString, amount);
-                    System.out.println(amount+" BRL equivalen a: "+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
-                    mostrarMenu();
-                    numeroMenu = Integer.valueOf(lectura.nextLine());
+                    ejecutarConversion(tipoDeMonedaString, numeroMenu, lectura, consulta, " BRL equivalen a: ", "BRL", "USD");
                 } else if (numeroMenu == 5) {
-                    tipoDeMonedaString = numeroMenu.toString().replace("5","COP");
-                    System.out.println("Escriba la cantidad que quiere convertir: ");
-                    var amount = Double.valueOf(lectura.nextLine());
-                    TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda("USD",tipoDeMonedaString, amount);
-                    System.out.println(amount+" USD equivalen a: "+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
-                    mostrarMenu();
-                    numeroMenu = Integer.valueOf(lectura.nextLine());
-                } else if ( numeroMenu == 6) {
-                    tipoDeMonedaString = numeroMenu.toString().replace("6","USD");
-                    System.out.println("Escriba la cantidad que quiere convertir: ");
-                    var amount = Double.valueOf(lectura.nextLine());
-                    TipoDeMoneda cantidad = consulta.consultaTipoDeMoneda("COP",tipoDeMonedaString, amount);
-                    System.out.println(amount+" COP equivalen a: "+cantidad.conversion_rate()+" "+tipoDeMonedaString+"\n"+separador+"\n");
-                    mostrarMenu();
-                    numeroMenu = Integer.valueOf(lectura.nextLine());
+                    ejecutarConversion(tipoDeMonedaString, numeroMenu, lectura, consulta, " USD equivalen a: ", "USD", "COP");
+                } else if (numeroMenu == 6) {
+                    ejecutarConversion(tipoDeMonedaString, numeroMenu, lectura, consulta, " COP equivalen a: ", "COP", "USD");
                 } else {
                     System.out.println("Opcion no valida \n");
                     mostrarMenu();
